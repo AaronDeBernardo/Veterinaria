fc25ce5fe5c3560369ba385435f626e4a1c6f005
DeBer24 <142444363+DeBer24@users.noreply.github.com>
2024-04-08 15:59:31 -0300

    Bug menuSuperior solucionado

--- menuSuperior.php
+++ menuSuperior.php
@@ -8,7 +8,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">
-            <img src="Recursos/logoVeterinaria.png" id="logo" class="d-inline-block align-top" alt="Logo veterinaria. Click para ir al inicio">
+            <img src="Recursos/logoVeterinaria.png" id="logoVeterinaria" class="d-inline-block align-top" alt="Logo veterinaria. Click para ir al inicio">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -18,7 +18,7 @@
             <div class="secciones d-flex flex-wrap">
                 <ul class="navbar-nav mr-auto mx-3 px-4">
                     <li class="nav-item active ">
-                        <img id="imgNuestraEmpresa" src="Recursos/logoNuestraEmpresa.png" alt="Nuestra Empresa">
+                        <img class="logo" src="Recursos/logoNuestraEmpresa.png" alt="Nuestra Empresa">
                         <a class="nav-link text-black" id="seccionesNavegacion" href="nuestraEmpresa.php">NUESTRA EMPRESA</a>
                     </li>
                 </ul>
@@ -27,7 +27,7 @@
             <div class="secciones d-flex flex-wrap ">
                 <ul class="navbar-nav mr-auto mx-3 px-4">
                     <li class="nav-item active">
-                        <img id="imgNuestraEmpresa" src="Recursos/logoServicios.png" alt="Servicios">
+                        <img class="logo" src="Recursos/logoServicios.png" alt="Servicios">
                         <a class="nav-link text-black" id="seccionesNavegacion" href="nuestrosServicios.php">SERVICIOS</a>
                     </li>
                 </ul>
@@ -37,12 +37,12 @@
             <div class="secciones d-flex flex-wrap">
                 <ul class="navbar-nav mr-auto mx-3 px-4">
                     <li class="nav-item active">
-                        <img id="imgNuestraEmpresa" src="recursos/logoUbicacion.png" alt="Ubicacion">
+                        <img class="logo" src="recursos/logoUbicacion.png" alt="Ubicacion">
                         <a class="nav-link text-black" id="seccionesNavegacion"  href="nuestraUbicacion.php">UBICACIÓN</a>
                     </li>
                 </ul>
             </div>
-        </div>    
+        </div>
         <div class="collapse navbar-collapse justify-content-end mx-5">
     <?php
         if (isset($_SESSION['rol'])){?>
