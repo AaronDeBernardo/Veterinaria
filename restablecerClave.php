dd278a230c4ac925d58a5217ebab35b3e95b990d
Milton_Borsato <borsatomilton@gmail.com>
2024-04-23 19:56:36 -0300

    reset clave

---
+++ restablecerClave.php
@@ -0,0 +1,30 @@
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reestablecer Contraseña</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+<div class="container-fluid">   
+    <div class="row d-flex justify-content-center mt-3">
+        <div class="col-12 col-md-6">
+            <h2>Modificar Contraseña</h2>
+            <form action="" method="POST">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Nueva contraseña</label>
+                    <input type="password" class="form-control" name="claveNueva" id="claveNueva" required>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Repetir nueva contraseña</label>
+                    <input type="password" class="form-control" name="claveRepetida" id="claveRepetida" required>
+                </div>
+                <button type="submit" class="btn btn-warning">Modificar Contraseña</button>
+            </form>
+        </div>
+    </div>  
+</div>   
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
